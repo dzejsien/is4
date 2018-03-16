90585f463f453d8823ad5e6e7bada66d80dc91db
Sebastian Bojarczak <Sebastian.Bojarczak@altkom.pl>
2018-03-16 16:02:14 +0100

    stores in db - init

--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using IdentityServer.Data;
 using IdentityServer.Models;
 using IdentityServer4;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -9,26 +11,28 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
+using System.Linq;
+using System.Reflection;
 /*
- * mialem problem z otnet ef database update -c ApplicationDbContext
- * - dodalem ref do Microsoft.EntityFrameworkCore.Tools, ale to nie pomoglo - nie wiem czy to ejst potrzebne na pewno
- * - dodalem do csproj:
- * <ItemGroup>   
- *  <DotNetCliToolReference Include="Microsoft.EntityFrameworkCore.Tools" Version="2.0.0" />   
- *  <DotNetCliToolReference Include="Microsoft.EntityFrameworkCore.Tools.DotNet" Version="2.0.0" /> 
- * </ItemGroup>
- * to dopiero pomoglo, usunalem ref - update bazy dziala
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
+* mialem problem z otnet ef database update -c ApplicationDbContext
+* - dodalem ref do Microsoft.EntityFrameworkCore.Tools, ale to nie pomoglo - nie wiem czy to ejst potrzebne na pewno
+* - dodalem do csproj:
+* <ItemGroup>   
+*  <DotNetCliToolReference Include="Microsoft.EntityFrameworkCore.Tools" Version="2.0.0" />   
+*  <DotNetCliToolReference Include="Microsoft.EntityFrameworkCore.Tools.DotNet" Version="2.0.0" /> 
+* </ItemGroup>
+* to dopiero pomoglo, usunalem ref - update bazy dziala
+* 
+* 
+* 
+* 
+* 
+* 
+* 
+* 
 
 
- */
+*/
 namespace IdentityServer
 {
     public class Startup
@@ -59,6 +63,8 @@ namespace IdentityServer
 
             services.AddMvc();
 
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
             services
                 .AddIdentityServer(options =>
                 {
@@ -68,10 +74,26 @@ namespace IdentityServer
                     options.Events.RaiseSuccessEvents = true;
                 })
                 .AddDeveloperSigningCredential()
-                .AddInMemoryPersistedGrants()
-                .AddInMemoryApiResources(Config.GetApiResources())
-                .AddInMemoryIdentityResources(Config.GetIdentityResources())
-                .AddInMemoryClients(Config.GetClients())
+                //.AddInMemoryPersistedGrants()
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = builder =>
+                        builder.UseSqlServer(Configuration.GetConnectionString("ISData"),
+                            sql => sql.MigrationsAssembly(migrationsAssembly));
+
+                    // this enables automatic token cleanup. this is optional.
+                    options.EnableTokenCleanup = true;
+                    options.TokenCleanupInterval = 30;
+                })
+                //.AddInMemoryApiResources(Config.GetApiResources())
+                //.AddInMemoryIdentityResources(Config.GetIdentityResources())
+                //.AddInMemoryClients(Config.GetClients())
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = builder =>
+                        builder.UseSqlServer(Configuration.GetConnectionString("ISData"),
+                            sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
                 //.AddTestUsers(Config.GetUsers());
                 .AddAspNetIdentity<ApplicationUser>();
 
@@ -103,6 +125,8 @@ namespace IdentityServer
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            InitializeDatabase(app);
+
             loggerFactory.AddConsole(LogLevel.Trace);
 
             if (env.IsDevelopment())
@@ -113,6 +137,47 @@ namespace IdentityServer
             app.UseIdentityServer();
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
+        }
+
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Config.GetClients())
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Config.GetIdentityResources())
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in Config.GetApiResources())
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
